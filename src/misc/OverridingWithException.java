f437fbd1023d14462e45ea4d3efb9dbab310d85e
Sourav Mittal <sourav.mittal05@gmail.com>
2025-08-17 23:55:55 +0530

    parent throwing exception and child is not throwing exception

--- src/misc/OverridingWithException.java
+++ src/misc/OverridingWithException.java
@@ -13,6 +13,9 @@ class Parent {
     public Number method3() {
         return null;
     }
+
+    public void method4() throws IOException {
+    }
 }
 
 class Child extends Parent {
@@ -41,7 +44,32 @@ class Child extends Parent {
     public Integer method3() {
         return null;
     }
+
+    @Override
+    public void method4() {
+    }
 }
 
 public class OverridingWithException {
+    public static void main(String[] args) {
+        Parent parent = new Child();
+        try {
+            parent.method1();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            parent.method4();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Child child = new Child();
+        try {
+            child.method1();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        child.method4();
+    }
 }
